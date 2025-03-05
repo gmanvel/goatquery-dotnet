da7bd1e2d01e632836eb25eda12af12063beab18
Manvel Ghazaryan <gh_manvel@yahoo.com>
2025-03-05 11:47:56 +0100

    Adding an async version of EnableQueryAttribute & updating controller example

---
+++ examples/controller/Migrations/20250305103438_Initial.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace controller.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Firstname = table.Column<string>(type: "varchar(128)", nullable: false),
+                    Lastname = table.Column<string>(type: "varchar(128)", nullable: false),
+                    Email = table.Column<string>(type: "varchar(320)", nullable: false),
+                    AvatarUrl = table.Column<string>(type: "varchar(1024)", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    UserName = table.Column<string>(type: "varchar(64)", nullable: false),
+                    PersonSex = table.Column<string>(type: "varchar(32)", nullable: false),
+                    Age = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ src/Attributes/EnableQueryAsyncAttribute.cs
@@ -0,0 +1,122 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.EntityFrameworkCore;
+
+public sealed class EnableQueryAsyncAttribute<T> : Attribute, IAsyncActionFilter
+{
+    private readonly int _topMax = 100;
+
+    public EnableQueryAsyncAttribute(int topMax)
+    {
+        _topMax = topMax;
+    }
+
+    public EnableQueryAsyncAttribute() { }
+
+    public async Task OnActionExecutionAsync(ActionExecutingContext _, ActionExecutionDelegate next)
+    {
+        var context = await next();
+
+        var result = context.Result as ObjectResult;
+        if (result is null) return;
+
+        var content = result.Value as IQueryable<T>;
+        if (content is null) return;
+
+        var queryString = context.HttpContext.Request.Query;
+
+        // Top
+        queryString.TryGetValue("top", out var topQuery);
+        var topString = topQuery.ToString();
+
+        bool parsedTop = int.TryParse(topString, out int top);
+
+        if (!string.IsNullOrEmpty(topString) && !parsedTop)
+        {
+            // Whatever was passed to as 'Top' was not parseable to a int
+            result.StatusCode = StatusCodes.Status400BadRequest;
+            result.Value = new QueryErrorResponse(StatusCodes.Status400BadRequest, "The query parameter 'Top' could not be parsed to an integer");
+
+            return;
+        }
+
+        // Skip
+        queryString.TryGetValue("skip", out var skipQuery);
+        var skipString = skipQuery.ToString();
+
+        bool parsedSkip = Int32.TryParse(skipString, out int skip);
+
+        if (!string.IsNullOrEmpty(skipString) && !parsedSkip)
+        {
+            // Whatever was passed to as 'Skip' was not parseable to a int
+            result.StatusCode = StatusCodes.Status400BadRequest;
+            result.Value = new QueryErrorResponse(StatusCodes.Status400BadRequest, "The query parameter 'Skip' could not be parsed to an integer");
+
+            return;
+        }
+
+        // Count
+        queryString.TryGetValue("count", out var countQuery);
+        var countString = countQuery.ToString();
+
+        bool parsedCount = bool.TryParse(countString, out bool count);
+
+        if (!string.IsNullOrEmpty(countString) && !parsedCount)
+        {
+            // Whatever was passed to as 'count' was not parseable to a bool
+            result.StatusCode = StatusCodes.Status400BadRequest;
+            result.Value = new QueryErrorResponse(StatusCodes.Status400BadRequest, "The query parameter 'Count' could not be parsed to a boolean");
+
+            return;
+        }
+
+        // Order by
+        queryString.TryGetValue("orderby", out var orderbyQuery);
+
+        // Select
+        queryString.TryGetValue("select", out var selectQuery);
+
+        // Search
+        queryString.TryGetValue("search", out var searchQuery);
+        var search = searchQuery.ToString();
+
+        // Filter
+        queryString.TryGetValue("filter", out var filterQuery);
+
+        var query = new Query()
+        {
+            Top = !string.IsNullOrEmpty(topString) ? top : _topMax,
+            Skip = skip,
+            Count = count,
+            OrderBy = orderbyQuery.ToString(),
+            Select = selectQuery.ToString(),
+            Search = search,
+            Filter = filterQuery.ToString()
+        };
+
+        ISearchBinder<T>? searchBinder = null;
+
+        if (!string.IsNullOrEmpty(search))
+        {
+            searchBinder = context.HttpContext.RequestServices.GetService(typeof(ISearchBinder<T>)) as ISearchBinder<T>;
+        }
+
+        try
+        {
+            var (queryableData, totalCount) = await content.ApplyAsync(query, _topMax, searchBinder);
+
+            var data = await ((IQueryable<object>)queryableData).ToListAsync();
+
+            result.StatusCode = StatusCodes.Status200OK;
+            // We use <object> here because when utilizing the 'select' functionality, it becomes
+            // an anonymous object and no longer is the type T.
+            result.Value = new PagedResponse<object>(data, totalCount);
+        }
+        catch (Exception ex)
+        {
+            result.StatusCode = StatusCodes.Status400BadRequest;
+            result.Value = new QueryErrorResponse(StatusCodes.Status400BadRequest, ex.Message);
+        }    
+    }
+}
